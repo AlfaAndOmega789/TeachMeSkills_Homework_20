99c104e1bbfff0fd8c61d2295d91adbaf30ecc03
Ivan <alfaandomega789@gmail.com>
2023-11-18 23:40:20 +0300

    init commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,36 +5,36 @@ import java.sql.*;
 import com.mysql.cj.jdbc.Driver;
 
 public class Main {
-    private static final String URL = "jdbc:mysql://localhost:3306/users?autoReconnect=true&useSSL=false";
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest?autoReconnect=true&useSSL=false";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
     public static void main(String[] args) throws SQLException {
 
-//        Driver driver = new Driver();
-//        DriverManager.registerDriver(driver);
+        Driver driver = new Driver();
+        DriverManager.registerDriver(driver);
 
         try (Connection conn =  DriverManager.getConnection(URL, USERNAME, PASSWORD)){
-//            Statement statement = conn.createStatement();
-//
+            Statement statement = conn.createStatement();
+
 //            statement.execute("""
-//                create table user(
+//                create table users(
 //                id integer primary key auto_increment, name varchar(100))
 //                """);
-//
-//            String sql = "INSERT INTO users (id, name) Values(?, ?)";
-//            PreparedStatement preparedStatement = conn.prepareStatement(sql);
-//            preparedStatement.setInt(1, 3);
-//            preparedStatement.setString(1, "Ivan");
-//            preparedStatement.execute();
-//
-//            ResultSet set = preparedStatement.executeQuery("select * from users");
-//
-//            while(set.next()){
-//                System.out.println(set.getInt("id") + " " + set.getString("name"));
-//            }
-//
-//            conn.setAutoCommit(true);
+
+            String sql = "INSERT INTO user (id, name) Values(?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setInt(1, 2);
+            preparedStatement.setString(2, "Ivan");
+            preparedStatement.execute();
+
+            ResultSet set = preparedStatement.executeQuery("select * from user");
+
+            while(set.next()){
+                System.out.println(set.getInt("id") + " " + set.getString("name"));
+            }
+
+            conn.setAutoCommit(true);
         } catch (SQLException ex) {
             System.out.println(ex);
             System.out.println("Неудалось загрузить класс драйвер");
