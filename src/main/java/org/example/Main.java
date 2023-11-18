67578d243942b1e7efa150843e39e166d05b24c6
Ivan <alfaandomega789@gmail.com>
2023-11-18 20:41:31 +0300

    init commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/sys";
+        String url = "jdbc:mysql://localhost:3306/users?autoReconnect=true&useSSL=false";
         String user = "root";
         String password = "root";
 
---
+++ src/main/java/org/resources/test.sql
@@ -0,0 +1,11 @@
+
+insert into users (name, age, email) values ('Ivan', 24, 'teach.me.skills70@gmail.com');
+
+select name, age from users where id = 2;
+
+select * from users;
+
+delete from users
+
+update users set name = 'Pavel', age = 30 where id = 1;
+
